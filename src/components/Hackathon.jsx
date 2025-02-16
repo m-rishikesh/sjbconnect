2375e97d95020003b5cd51fbf6b4d7d30c89c16a
rajubuntu <raj@raj.com>
2025-02-16 15:45:33 +0530

    adds the Link in the Hackathon.jsx

--- src/components/Hackathon.jsx
+++ src/components/Hackathon.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import EventCard from './EventCard';  // Import EventCard Component
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Hackathon = () => {
   // const events = [
